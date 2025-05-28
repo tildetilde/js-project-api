62b69523c184d659f792fc4417647a813b001284
tildetilde <tilde.egebrand@gmail.com>
2025-05-28 15:27:05 +0200

    Built pagination based on pages and limit

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,13 +1,27 @@
 // controllers/thoughtsController.js
 import loadThoughts from "../utils/loadThoughts.js";
 
-// @desc Get all thoughts
+// Get all thoughts
 export const getAllThoughts = (req, res) => {
   const thoughts = loadThoughts();
-  res.json(thoughts);
+
+  const page = parseInt(req.query.page) || 1;
+  const limit = parseInt(req.query.limit) || 10;
+
+  const startIndex = (page - 1) * limit;
+  const endIndex = startIndex + limit;
+
+  const paginatedThoughts = thoughts.slice(startIndex, endIndex);
+
+  res.json({
+    totalThoughts: thoughts.length,
+    totalPages: Math.ceil(thoughts.length / limit),
+    currentPage: page,
+    thoughts: paginatedThoughts,
+  });
 };
 
-// @desc Get a single thought by ID
+// Get a single thought by ID
 export const getThoughtById = (req, res) => {
   const { id } = req.params;
   const thoughts = loadThoughts();
