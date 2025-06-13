3509abf81c5a107787ecccc8ff2e43938bfebad7
tildetilde <tilde.egebrand@gmail.com>
2025-06-13 11:01:48 +0200

    regex validation

--- models/User.js
+++ models/User.js
@@ -2,7 +2,15 @@ import mongoose from "mongoose";
 import bcrypt from "bcryptjs";
 
 const UserSchema = new mongoose.Schema({
-  email: { type: String, unique: true, required: true },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true,
+    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
+  },
+
   password: { type: String, required: true },
   username: { type: String },
 });
