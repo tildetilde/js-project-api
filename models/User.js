c18908272011757759dd49fcd734ebf61e6b13d2
tildetilde <tilde.egebrand@gmail.com>
2025-06-13 12:01:16 +0200

    Updated user

--- models/User.js
+++ models/User.js
@@ -6,9 +6,6 @@ const UserSchema = new mongoose.Schema({
     type: String,
     required: true,
     unique: true,
-    lowercase: true,
-    trim: true,
-    match: [/^\S+@\S+\.\S+$/, "Please use a valid email address"],
   },
 
   password: { type: String, required: true },
